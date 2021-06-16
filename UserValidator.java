e31e840d6b5022e26870b0e24f3dc6019ac8f710
Ranjithgc <ranjith.amca.16@acharya.ac.in>
2021-06-16 13:20:14 +0530

    Validating Password contain atleast 1 number

--- UserValidator.java
+++ UserValidator.java
@@ -13,7 +13,7 @@ public class UserValidator {
 	private final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,10}$"; 
 	private final String EMAIL_PATTERN = "^[a-zA-Z0-9]{1,}[[.|+|-]a-zA-Z0-9]*@[a-zA-Z0-9]{1,}((.){1}+)([a-z]{1,3}+)(.[a-z]{2,3})*$";
 	private final String PHONE_NUMBER_PATTERN = "[0-9]{2}[\\s][0-9]{10}$";
-	private	final String PASSWORD_PATTERN = "^[a-zA-Z]{8,}"
+	private	final String PASSWORD_PATTERN = "^[a-zA-Z0-9]{8,}"
 
 	/** 
   	 * Uc1:
@@ -71,6 +71,8 @@ public class UserValidator {
 	* Rule 1: Should contain atleast 8 character
 	* Uc6:
 	* Rule 2: should contain atleast 1 Uppercase letter
+	* Uc7:
+	* Rule 3: should contain atleast 1 number
      	*/
 
     	public boolean validatePassword(String password) {
